65c0e1e2d16fdc8f000a82e702a8b95d18d3569d
rji1 <rinehart.ji@rockwellautomation.com>
2023-03-09 12:55:23 +0800

    Use CommunityToolkit.Mvvm

---
+++ JBInspectCodeProject/MyViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace JBInspectCodeProject;
+
+public partial class MyViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string? _name;
+}
